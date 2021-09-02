75b7fb8f43cb39f1d34f6cb36c5536bac4e88489
Irman Omerovic <92omir1mst@hft-stuttgart.de>
2021-09-02 23:29:27 +0000

    output benchmark to stdio instead of file because IOTLAB doesnt allow file access/creation

--- aes-ecb.c
+++ aes-ecb.c
@@ -8,6 +8,7 @@
 
 void executeAesEcb(char* message, int currentRound, FILE* filePointer) {
     cipher_t cipher;
+    (void)(filePointer);
     int dataLength = strlen(message);
     uint8_t* input = malloc(dataLength);
     memmove(input, message, dataLength);
@@ -18,26 +19,27 @@ void executeAesEcb(char* message, int currentRound, FILE* filePointer) {
     uint8_t* output = calloc(requiredBlocks, AES_BLOCK_SIZE);
     uint8_t* key = malloc(KEY_SIZE);
     random_bytes(key, KEY_SIZE);
-    printf("Key\n");
-    od_hex_dump(key, KEY_SIZE, 0);
+    //printf("Key\n");
+    //od_hex_dump(key, KEY_SIZE, 0);
     sprintf((char *)input, "%s", message);
     cipher_init(&cipher, CIPHER_AES, key, KEY_SIZE);
     uint32_t _benchmark_time_encryption = xtimer_now_usec();
     cipher_encrypt(&cipher, input, output);
     _benchmark_time_encryption = (xtimer_now_usec() - _benchmark_time_encryption);
-    printf("Plaintext: \t");
-    od_hex_dump(input, AES_BLOCK_SIZE, 0);
-    printf("Ciphertext: \t");
-    od_hex_dump(output, AES_BLOCK_SIZE, 0);
-    printf("Decrypted text: \t");
+    //printf("Plaintext: \t");
+    //od_hex_dump(input, AES_BLOCK_SIZE, 0);
+    //printf("Ciphertext: \t");
+    //od_hex_dump(output, AES_BLOCK_SIZE, 0);
+    //printf("Decrypted text: \t");
     uint32_t _benchmark_time_decryption = xtimer_now_usec();
     cipher_decrypt(&cipher, output, input);
     _benchmark_time_decryption = (xtimer_now_usec() - _benchmark_time_decryption);
-    od_hex_dump(input, AES_BLOCK_SIZE, 0);
+    //od_hex_dump(input, AES_BLOCK_SIZE, 0);
     uint32_t benchmarkResults[2];
     benchmarkResults[0] = _benchmark_time_encryption;
     benchmarkResults[1] = _benchmark_time_decryption;
-    benchmark_save_runtime(benchmarkResults, currentRound, filePointer);
+    //benchmark_save_runtime(benchmarkResults, currentRound, filePointer);
+    printf(";%d;%u;%u\n", currentRound, benchmarkResults[0], benchmarkResults[1]);
     free(input);
     free(output);
     free(key);
--- main.c
+++ main.c
@@ -5,21 +5,21 @@
 #include "time.h"
 
 
-#define ROUNDS 100
+#define ROUNDS 1000
 #define MESSAGE_LENGTH 100
 
 int main(void)
 {
-    printf("Number of rounds: %d\n", ROUNDS);
+    printf("Starting Benchmark ...\nNumber of rounds: %d\nWordlength: %d", ROUNDS, MESSAGE_LENGTH);
     FILE* filePointer = createBenchmarkFile("aes-ecb-128");
     for(int round = 1; round <= ROUNDS; round++){
         char* message = (char*)malloc(MESSAGE_LENGTH * sizeof(char) + 1);
         generateMessage(message, MESSAGE_LENGTH);
-        printf("Round %d of %d\nMessage to encrypt/decrypt: %s\n", round, ROUNDS, message);
+        //printf("Round %d of %d\nMessage to encrypt/decrypt: %s\n", round, ROUNDS, message);
         executeAesEcb(message, round, filePointer);
         free(message);
     }
-    printf("Benchmark done!\nClosing file access ...\n");
+    printf("Benchmark done!\n");
     fclose(filePointer);
     return 0;
 }
